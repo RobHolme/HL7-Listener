e9e06420f3062915f5ce69b0b6c7d5418827d838
Rob Holme <rob@holme.com.au>
2022-10-01 11:49:20 +0800

    added publish commands. .Net 6 releases are self-contained, targeting win-x64 and linux-x64

---
+++ build-publish.cmd
@@ -0,0 +1,6 @@
+del .\bin\publish\net6.0\win-x64\
+del .\bin\publish\net6.0\linux-x64\
+del  .\bin\publish\net4.8\
+dotnet publish --configuration release --framework net6.0 --runtime win-x64 --self-contained --output .\bin\publish\net6.0\win-x64\
+dotnet publish --configuration release --framework net6.0 --runtime linux-x64 --self-contained --output .\bin\publish\net6.0\linux-x64\
+dotnet publish --configuration release --framework net48 --output  .\bin\publish\net4.8\
