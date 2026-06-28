4d4cf60ac080ba7996d798c0a9d8184e3eb3b9b3
Rob Holme <rob@holme.com.au>
2026-06-28 16:12:56 +0800

    updated to target .net10

--- build-publish.cmd
+++ build-publish.cmd
@@ -1,2 +1,2 @@
-dotnet publish --configuration release --framework net6.0 --runtime win-x64 --self-contained -p:PublishSingleFile=true --output .\bin\publish\win-x64\ 
-dotnet publish --configuration release --framework net6.0 --runtime linux-x64 --self-contained -p:PublishSingleFile=true --output .\bin\publish\linux-x64\
+dotnet publish --configuration release --framework net10.0 --runtime win-x64 --self-contained -p:PublishSingleFile=true --output .\bin\publish\win-x64\ 
+dotnet publish --configuration release --framework net10.0 --runtime linux-x64 --self-contained -p:PublishSingleFile=true --output .\bin\publish\linux-x64\
